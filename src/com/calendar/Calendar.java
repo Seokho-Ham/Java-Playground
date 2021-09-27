ad9c538c0d2eedd000cfa1cb131532d10fe072c3
Seokho-Ham <seokho9522@gmail.com>
2021-09-27 16:52:09 +0900

    - Step5, Step6, Step7

--- src/com/calendar/Calendar.java
+++ src/com/calendar/Calendar.java
@@ -3,6 +3,8 @@ package com.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+    private static final String PROMPT = "> ";
+
     //Step 2. 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
     public static void step2(Scanner scanner) {
         System.out.println("월을 입력하세요.");
@@ -11,7 +13,7 @@ public class Calendar {
         if (month < 1 || month > 12) {
             System.out.println("1월부터 12월 사이의 월을 입력해주세요.");
         } else {
-            System.out.printf("%d월은 %d까지 있습니다.", month, MonthList.getLastDay(month - 1));
+            System.out.printf("%d월은 %d까지 있습니다.", month, MonthList.getLastDay(2021,month - 1));
         }
     }
 
@@ -26,42 +28,92 @@ public class Calendar {
             months[i] = month;
         }
         for (int i = 0; i < months.length; i++) {
-            System.out.printf("%d월은 %d일까지 있습니다.\n", months[i], MonthList.getLastDay(months[i]));
+            System.out.printf("%d월은 %d일까지 있습니다.\n", months[i], MonthList.getLastDay(2021,months[i]));
         }
 
     }
 
+    //Step 4. 반복적으로 월을 입력할 수 있도록 한다.
     public static void step4(Scanner scanner) {
         while (true) {
             System.out.println("월을 입력해주세요.");
-            System.out.print("> ");
+            System.out.print(PROMPT);
             int inputMonth = scanner.nextInt();
             if (inputMonth == -1) {
                 System.out.println("Have a nice day!");
                 break;
             } else {
-                System.out.printf("%d월은 %d일까지 있습니다. \n", inputMonth, MonthList.getLastDay(inputMonth));
+                System.out.printf("%d월은 %d일까지 있습니다. \n", inputMonth, MonthList.getLastDay(2021,inputMonth));
             }
         }
 
     }
 
-    public static void main(String[] args) {
-        //Step 1. 달력을 출력해라
-        System.out.printf("%3s%3s%3s%3s%3s%3s%3s%n", "일", "월", "화", "수", "목", "금", "토");
-        System.out.println("----------------------");
-        for (int i = 1; i < 29; i++) {
-            if (i % 7 == 0) {
-                System.out.printf("%3d%n", i);
+    //Step 5. input으로 받은 월의 달력을 출력해라.
+    //1일은 일요일로 정해도 된다.
+    public static void step5(Scanner scanner) {
+        while (true) {
+            System.out.println("월을 입력해주세요.");
+            System.out.print(PROMPT);
+            int inputMonth = scanner.nextInt();
+            if (inputMonth == -1) {
+                System.out.println("Have a nice day!");
+                break;
             } else {
-                System.out.printf("%3d", i);
+                MonthList.printMonth(2021, inputMonth);
+                System.out.println("\n");
             }
         }
+    }
+
+    //Step 6. 입력받은 요일을 1일로 설정해서 달력 출력하기.
+    public static void step6(Scanner scanner) {
+        while (true) {
+            System.out.println("년을 입력해주세요.");
+            System.out.print(PROMPT);
+            int inputYear = scanner.nextInt();
+            if (inputYear == -1) {
+                System.out.println("Have a nice day!");
+                break;
+            }
+            System.out.println("월을 입력해주세요.");
+            System.out.print(PROMPT);
+            int inputMonth = scanner.nextInt();
+            if (inputMonth == -1) {
+                System.out.println("Have a nice day!");
+                break;
+            }
+            System.out.println("시작일을 골라주세요.");
+            System.out.println("1.SUN | 2.MON | 3.TUE | 4.WED | 5.THU | 6.FRI | 7.SAT");
+            int inputDay = scanner.nextInt();
+            MonthList.printMonth(inputYear, inputMonth, inputDay - 1);
+            System.out.println("\n");
+        }
+    }
+
+
+    //Step 7. 년, 월만 입력받아서 정확한 달력을 출력할 것.
+    //특정 년도에서 그 다음해 같은 월 같은 일일 경우 해당 요일은 하루 밀린다.
+    //윤년일 경우 이틀이 밀린다.
+    public static void step7(Scanner scanner){
+        System.out.println("년도를 입력해주세요.");
+        int year = scanner.nextInt();
+        System.out.println("월을 입력해주세요.");
+        int month = scanner.nextInt();
+        MonthList.printMonth(year,month);
+
+    }
+    public static void main(String[] args) {
+        //Step 1. 달력을 출력해라
+
         Scanner scanner = new Scanner(System.in);
 
 //        step2(scanner);
 //        step3(scanner);
-        step4(scanner);
+//        step4(scanner);
+//        step5(scanner);
+//        step6(scanner);
+        step7(scanner);
     }
 
 }
--- src/com/calendar/MonthList.java
+++ src/com/calendar/MonthList.java
@@ -1,8 +1,65 @@
 package com.calendar;
 
+
 public class MonthList {
-    private static final int[] list = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    public static int getLastDay(int month){
-        return list[month-1];
-    };
+    private static final int[] MONTH_LIST = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] LEAPYEAR_MONTH_LIST = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static int getLastDay(int year, int month) {
+        if (checkLeapYear(year)) {
+            return LEAPYEAR_MONTH_LIST[month - 1];
+        }
+        return MONTH_LIST[month - 1];
+    }
+
+    ;
+
+    public static void printMonth(int year, int month) {
+        System.out.printf("<<%4d %3d>>\n", year, month);
+        System.out.println(" SUN MON TUE WED THU FRI SAT");
+        System.out.println("----------------------------");
+        int day = calculateStartDay(year)-1;
+        int startDate = 7 - day;
+        int endDate = startDate < 7 ? startDate : 0;
+        for (int i = 0; i < day; i++) {
+            System.out.print("    ");
+        }
+        for (int i = 1; i <= getLastDay(year, month); i++) {
+            System.out.printf("%4d", i);
+            if (i % 7 == endDate) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void printMonth(int year, int month, int day) {
+        System.out.printf("<<%4d %3d>>\n", year, month);
+        System.out.println(" SUN MON TUE WED THU FRI SAT");
+        System.out.println("----------------------------");
+        int startDate = 7 - day;
+        int endDate = startDate < 7 ? startDate : 0;
+        for (int i = 0; i < day; i++) {
+            System.out.print("    ");
+        }
+        for (int i = 1; i <= getLastDay(year, month); i++) {
+            System.out.printf("%4d", i);
+            if (i % 7 == endDate) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static boolean checkLeapYear(int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int calculateStartDay(int year) {
+        int fullYear = year - 1;
+        int result = fullYear + fullYear / 4 - fullYear / 10 + fullYear / 400;
+
+        return result % 7;
+    }
 }
