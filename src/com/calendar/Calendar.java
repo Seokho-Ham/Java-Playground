d2fb6261d3fdb7cae5fa1951aaaa4ee8d5c381ed
Seokho-Ham <seokho9522@gmail.com>
2021-09-27 12:48:15 +0900

    Calender - Step 3

--- src/com/calendar/Calendar.java
+++ src/com/calendar/Calendar.java
@@ -18,7 +18,8 @@ public class Calendar {
 
         //Step 2. 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         int[] monthList = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        System.out.println("월을 입력하세요.");
+/*        System.out.println("월을 입력하세요.");
+/*        System.out.println("월을 입력하세요.");
         System.out.print("> ");
         Scanner scanner = new Scanner(System.in);
         int month = scanner.nextInt();
@@ -26,8 +27,21 @@ public class Calendar {
             System.out.println("1월부터 12월 사이의 월을 입력해주세요.");
         } else {
             System.out.printf("%d월은 %d까지 있습니다.", month, monthList[month - 1]);
-        }
+        }*/
 
+        //Step 3. 원하는 횟수만큼 입력을 받고 해당 월의 마지막일을 출력하기
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("반복 횟수를 입력하세요.");
+        int repeatTimes = scanner.nextInt();
+        int[] months = new int[repeatTimes];
+        System.out.println("월을 입력하세요.");
+        for(int i=0; i<repeatTimes; i++){
+            int month = scanner.nextInt();
+            months[i] = month;
+        }
+        for(int i=0; i<months.length; i++){
+            System.out.printf("%d월은 %d일까지 있습니다.\n",months[i], monthList[months[i] - 1]);
+        }
 
     }
 }
